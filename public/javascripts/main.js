7625ca40b78961ff8562732d2ae7ef28abb9634c
hasa93 <hasanga.charaka@gmail.com>
2015-10-04 00:05:05 +0530

    Image upload through icon

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,12 @@
 
-function openFiles()
-{
-	$('#uploader').trigger('click');
+var prevUploaded = false;
+
+function handleFiles(){
+
+	if(prevUploaded === false)
+		$('#file').trigger('click');	
+	else
+		$('#submitter').trigger('click');
+
+	prevUploaded = !prevUploaded;
 }
